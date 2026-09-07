f5218fb5870487032c28cb15a451738a06025e93
Mikhail Bobrutskov <wizzardobox@gmail.com>
2026-09-07 19:27:23 +0200

    added loop and reverseOnLoop methods

--- src/main/java/com/wizzardo/jme/transition/TransitionControl.java
+++ src/main/java/com/wizzardo/jme/transition/TransitionControl.java
@@ -7,14 +7,37 @@ import com.jme3.scene.control.AbstractControl;
 public class TransitionControl extends AbstractControl {
 
     protected final Transition transition;
+    protected boolean loop;
+    protected boolean reverseOnLoop;
 
     public TransitionControl(Transition transition) {
         this.transition = transition;
-        transition.onEnd(() -> spatial.removeControl(this));
+        transition.onEnd(() -> {
+            if (!loop) {
+                if (spatial != null)
+                    spatial.removeControl(this);
+            } else {
+                if (reverseOnLoop) {
+                    transition.reverse(!transition.reverse);
+                } else {
+                    transition.elapsedTime = transition.reverse ? transition.duration : 0;
+                }
+            }
+        });
     }
 
     public <T> TransitionControl(Change<T> change, float duration, Interpolation<T> interpolation, EasingFunction easingFunction) {
         this(new Transition<>(change, duration, interpolation, easingFunction));
+    }
+
+    public TransitionControl loop(boolean loop) {
+        this.loop = loop;
+        return this;
+    }
+
+    public TransitionControl reverseOnLoop(boolean reverseOnLoop) {
+        this.reverseOnLoop = reverseOnLoop;
+        return this;
     }
 
     @Override
